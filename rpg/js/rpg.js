9ddd0d6582125ea4a6a152ce7209375f2af054cb
Jeff <jtfennell@hotmail.com>
2014-10-28 19:04:55 -0700

    added username storage in local storage / login error checking

--- rpg/js/rpg.js
+++ rpg/js/rpg.js
@@ -1,8 +1,7 @@
+$(function(){
 /**
 to work on:
-	-look up character by name, rather than by ID
 	-create right click menu
-	-use animate css
 	-login menu
 		-local storage for username
 
@@ -11,6 +10,7 @@ var $searchButton = $('#searchButton');
 var $searchDiv = $('#searchDiv');
 var $searchBar = $('#searchBar');
 var $charSearchBtn = $('#charSearchBtn');
+var $logInBtn = $("#logInBtn");
 
 var openSearchBar = function(){
 	$searchDiv.show();
@@ -20,8 +20,8 @@ var openSearchBar = function(){
 }
 
 var checkIfUserLoggedIn = function(){
-	if(localStorage.getItem("userLoggedIn") === null){
-		promptUserForLogin();
+	if(localStorage.getItem("userName") !== null){
+		fillInUserName();
 	};
 }
 
@@ -32,10 +32,6 @@ var closeSearchBar = function(){
 		$searchDiv.hide();
 		$searchButton.show();
 	}
-}
-
-var promptUserForLogin = function(){
-
 }
 
 var getCharacter = function(){
@@ -59,18 +55,68 @@ var addCharacter = function(){
 
 }
 
+var editCharacter = function(){
+
+}
+
+
+//creates notification for when a character is added, deleted, edited, etc
+var editNotification = function(){
+
+}
+
 var getCharacterId = function(characterName){
 	$.getJSON(
     "http://lmu-diabolical.appspot.com/characters",
     function (characters) {
-        // Do something with the character list.
-        characters.forEach(function (character) {
-            console.log(character);
-        });
+      // Do something with the character list.
+      characters.forEach(function (character) {
+        console.log(character);
+      });
     }
-);
+	);
 }
 
+var checkIfUserLoggedIn = function(){
+	var userName = localStorage.getItem("userName")
+	
+	if (userName !== null) {
+		fillInUserName(userName);
+	};
+}
+
+var fillInUserName = function(userName){
+	$("#userInfoLogin").empty().text("Welcome back, " + userName);
+	$('#userInfoLogin').addClass('navbar-btn user-greeting');
+}
+
+var getUserName = function(){
+	var $userNameInput = $('#userNameInput');
+	var userName = $userNameInput.val();
+	
+	if (userName === "") {
+		$userNameInput.parent().addClass("has-error")
+		return;
+	};
+	if (userName !== "") {
+		localStorage.setItem("userName", userName);
+		fillInUserName(userName);
+	};
+}
+
+var clearLocalStorage = function(){
+	localStorage.removeItem("userName");
+}
+
+var hideLoginModal = function(){
+	$('#closeModal').click();
+}
+
+$(document).ready(checkIfUserLoggedIn());
 $searchButton.mouseenter(openSearchBar);
 $searchDiv.mouseleave(closeSearchBar);
 $charSearchBtn.click(getCharacter);
+$('#submitLogInBtn').click(getUserName);
+$('#submitLogInBtn').click(hideLoginModal);
+$('#cls').click(clearLocalStorage);
+})
