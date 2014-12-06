c3c3309211cd2f46bb60a9ce13c8d6f497f3ff04
Jeff <jtfennell@hotmail.com>
2014-12-06 00:37:32 -0800

    can now slide slider

--- widget-from-scratch/slider.js
+++ widget-from-scratch/slider.js
@@ -3,9 +3,11 @@
         var $receivingInput = this;
         var LEFT_ARROW_KEY = 37;
         var RIGHT_ARROW_KEY = 39;
+        var LEFT_MOUSE_BUTTON = 1;
         var DEFAULT_MIN = 0;
         var DEFAULT_MAX = 100;
-        var DEFAULT_KNOB_COLOR = '#70DB70';
+        var DEFAULT_KNOB_COLOR = 'black';
+        var sliderInitialPosition;
 
         var settings = $.extend({
             min:DEFAULT_MIN,
@@ -13,14 +15,50 @@
             color: DEFAULT_KNOB_COLOR
         }, options)
         
+        var sliderInfo = {}
+
         var checkForArrowKey = function (event) {
             if (event.keyCode === LEFT_ARROW_KEY) {
                 //decrease input, adjust position of slider knob
-                
+                alert('left arrow key pressed');
             }else if (event.keyCode === RIGHT_ARROW_KEY) {
                 //increment input, adjust position of slider knob
-                
+                alert('right arrow key pressed');
             }
+        }
+
+        var startDrag = function(event) {
+            if (event.which === LEFT_MOUSE_BUTTON) {
+               
+                //record initial position of slider
+                sliderInfo.initialPosition = ($(this).offset().left);
+                sliderInfo.active = true;
+            }; 
+        }
+
+        var endDrag = function () {
+            sliderInfo.active = false;
+        }
+
+        var pointerGrab = function (event) {
+            if (event.which === LEFT_MOUSE_BUTTON) {
+                this.classList.add('grabbing');
+            };
+            
+        }
+
+        var trackDrag = function () {
+            if (sliderInfo.active) {
+                console.log($(this));
+                $(this).offset({
+                    top: $(this).offset().top,
+                    left: event.pageX
+                })
+            };
+        }
+
+        var pointerHand = function () {
+            this.classList.remove('grabbing');
         }
 
         //change the type of input 
@@ -32,18 +70,18 @@
         //create components for slider
         $sliderContainer = $('<div></div>').addClass('slider-container');
         $sliderTrack = $('<div></div>').addClass('slider-track absolute-center');
-        $sliderGlyph = $('<span></span>').addClass('glyphicon glyphicon-transfer')
-        $sliderKnob = $('<div></div>').addClass('slider-knob').css('border', '2px solid black');
+        $sliderGlyph = $('<span></span>').addClass('glyphicon glyphicon-transfer arrow-glyph')
+        $sliderKnob = $('<div></div>').addClass('slider-knob').css('border', '2px solid ' + settings.color);
         $sliderContainer.append($sliderTrack.append($sliderKnob.append($sliderGlyph)));
 
         //append the slider beneath the input
         $receivingInput.parent().append($sliderContainer);
         
-        $sliderKnob.on('mousedown', trackDrag);
-        
-        var trackDrag = function() {
-
-        }
+        $sliderKnob.on('mousedown', startDrag);
+        $sliderKnob.on('mousedown', pointerGrab);
+        $sliderKnob.on('mouseup', pointerHand);
+        $sliderKnob.on('mouseup', endDrag);
+        $sliderKnob.on('mousemove', trackDrag);
 
         this.text('Slider');
         return this;
